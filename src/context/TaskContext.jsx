7c6920bd9410b401c6d6f1238f61a9b6b2089622
Ed.Responsivo <58023476+edresponsivo@users.noreply.github.com>
2022-10-26 09:52:41 -0700

    first-commit

---
+++ src/context/TaskContext.jsx
@@ -0,0 +1,39 @@
+import { createContext, useState, useEffect } from 'react';
+import { tasks as data } from '../data/tasks';
+
+export const TaskContext = createContext();
+
+export function TaskContextProvider(props) {
+  const [tasks, setTasks] = useState([]);
+
+  function createTask(task) {
+    const newTask = {
+      title: task.title,
+      id: tasks.length,
+      description: task.description,
+    };
+    setTasks([...tasks, newTask]);
+  }
+
+  function deleteTask(taskId) {
+    setTasks(tasks.filter((task) => task.id !== taskId));
+    // console.log(tasks);
+    // console.log(taskId);
+  }
+
+  useEffect(() => {
+    setTasks(data);
+  }, []);
+
+  return (
+    <TaskContext.Provider
+      value={{
+        tasks,
+        deleteTask,
+        createTask,
+      }}
+    >
+      {props.children}
+    </TaskContext.Provider>
+  );
+}
---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+  {
+    id: 0,
+    title: 'Mi primer tarea',
+    description: 'Primer tarea description...',
+  },
+  {
+    id: 1,
+    title: 'Mi Segunda tarea',
+    description: 'Segunda tarea description...',
+  },
+  {
+    id: 2,
+    title: 'Mi Tercer tarea',
+    description: 'Tercer tarea description...',
+  },
+];
